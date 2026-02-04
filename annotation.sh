55728d6651cd19828c0a43261548ef22b1abba9e
stolls01 <a12030007@unet.univie.ac.at>
2026-02-04 14:06:03 +0100

    Update annotation.sh

--- annotation.sh
+++ annotation.sh
@@ -10,14 +10,15 @@
 #SBATCH --mail-type=END,FAIL
 #SBATCH --mail-user=a12030007@unet.univie.ac.at
 
-### ENVIRONMENT
+### ENVIRONMENT AND VARIABLES
 module load StringTie
 
 wd="/lisc/data/scratch/course/2025w300106/stoll"
 inBam=$wd/results/map/SRR36696742_filtered_trimmed_HldesRot8A_Aligned.sortedByCoord.out.bam
 
 
-#the command takes an input bam file from specified location, output into results.
-#-l will add a prefix, -p to use 4 cores,
-#-j to increase necessary depth to detect a junctin, -g to increase allowed gap between junctions from default. -m
+#the command takes an input bam file from specified location, puts output into results.
+#-l will add a prefix to output transcripts, -p to use 4 cores,
+#-j to increase necessary depth to detect a junction, -g to increase allowed gap between junctions from default
+# -m to restrict minimum transcript length to 200, here upped from default 50 because my dataset contains mostly longer fragments
 stringtie $inBam -o $wd/results/stringtie_HldesRot8A.gtf -l HldesRot8A -p 4 -j 10 -g 200 -m 200
